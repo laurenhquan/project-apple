fb06d1c155039e12a0ab40bbccdf6a5bb28d498e
ananyakarthi21 <ananyakarthi@csu.fullerton.edu>
2025-03-02 20:04:06 -0800

    implemented flask, still trying to get it to work; DO NOT MERGE TO MAIN BRANCH, it is still not working fully

--- back-end/app.py
+++ back-end/app.py
@@ -1,16 +1,12 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, send_from_directory
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='front-end/pages')  # Serve static files from 'front-end/pages'
 
+# Route for home page rendering the index.html
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/get_image')
-def get_image():
-    rating = request.args.get('rating', default=1, type=int)
-    image_path = f"../images/imagesrating_{rating}.png"
-    return jsonify({"image": image_path})
+def home():
+    return render_template('index.html')  # Make sure index.html is in 'back-end/templates'
 
 if __name__ == '__main__':
     app.run(debug=True)
