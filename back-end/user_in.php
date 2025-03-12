5b91be7e2a53e239b60e541b14b933d0030b9437
laurenhquan <laurenhquan@gmail.com>
2025-03-12 13:04:15 -0700

    trying to fix insert problem

--- back-end/user_in.php
+++ back-end/user_in.php
@@ -4,9 +4,9 @@
     if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $mysqli = require __DIR__ . "./database.php"; // connects to database
 
-        $check_for_user = sprintf("SELECT * FROM user WHERE user_name = '%s'", $mysqli->real_escape_string($_POST["user_name"])); // query to check if user exists in db
+        $query = sprintf("SELECT * FROM user WHERE user_name = '%s'", $mysqli->real_escape_string($_POST["user_name"])); // query to check if user exists in db
 
-        $result = $mysqli->query($check_for_user); // run query
+        $result = $mysqli->query($query); // run query
 
         $existing_user = $result->fetch_assoc(); // variable to hold data if user exists
 
@@ -26,11 +26,11 @@
         else { // new user
             $pass_hash = password_hash($_POST["pass"], PASSWORD_DEFAULT); // hash password
 
-            $insert_user = "INSERT INTO user (user_name, pass_hash) VALUES (?, ?)"; // query to insert new user into db
+            $query = "INSERT INTO user (user_name, pass_hash) VALUES (?, ?)"; // query to insert new user into db
 
             $stmt = $mysqli->stmt_init(); // create statement
 
-            if(!$stmt->prepare($insert_user)) { die("MySQL error: " . $mysqli->error); } // kill if error with preparing query
+            if(!$stmt->prepare($query)) { die("MySQL error: " . $mysqli->error); } // kill if error with preparing query
 
             $stmt->bind_param("ss", $_POST["user_name"], $pass_hash); // binds parameters
 
@@ -38,9 +38,9 @@
                 session_start(); // start session aka user is logged in
 
                 // set session data to user's data
-                $_SESSION["user_id"] = $existing_user["user_id"];
-                $_SESSION["user_name"] = $existing_user["user_name"];
-                $_SESSION["pass_hash"] = $existing_user["pass_hash"];
+                $_SESSION["user_id"] = $_POST["user_id"];
+                $_SESSION["user_name"] = $_POST["user_name"];
+                $_SESSION["pass_hash"] = $_POST["pass_hash"];
 
                 header("Location: front-end/pages/logged-in.html"); // directs user to home page
                 exit;
