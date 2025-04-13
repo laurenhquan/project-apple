63ed0b701097134fbad7a854c7723fe1f184991f
laurenhquan <laurenhquan@gmail.com>
2025-04-13 03:06:19 -0700

    username now able to change successfully

--- back-end/change-profile.php
+++ back-end/change-profile.php
@@ -10,7 +10,7 @@
             }
             else { // if password is correct
                 if($_POST["new_username"] == $_SESSION["username"]) { // if user puts in the same username already using
-                    echo "<em style='color: #c1121f;'>You are already using this username</em><br>";
+                    echo "<em style='color: #c1121f;'>Username is already in use</em><br>";
                 }
                 else {
                     $check_user = sprintf("SELECT * FROM users WHERE username = '%s'", $mysqli->real_escape_string($_POST["new_username"])); // query to check if username exists in db
@@ -21,13 +21,14 @@
                         echo "<em style='color: #c1121f;'>Username is already taken</em><br>";
                     }
                     else { // if username is free
-                        $update_user = "UPDATE users SET username = ? WHERE id = ?"; // query to update username in db
+                        $update_user = "UPDATE users SET username = ? WHERE user_id = ?"; // query to update username in db
                         $stmt = $mysqli->stmt_init(); // create statement
                         if(!$stmt->prepare($update_user)) { die("MySQL error: " . $mysqli->error); } // kill if error with preparing query
-                        $stmt->bind_param("si", $_POST["new_username"], $_SESSION['user_id']); // binds parameters
+                        $stmt->bind_param("si", $_POST['new_username'], $_SESSION['user_id']); // binds parameters
 
                         if($stmt->execute()) { // execute statement
                             $_SESSION["username"] = $_POST["new_username"]; // update session username
+                            echo "<em style='color: #8db600;'>Username changed successfully</em><br>";
                             header("Location: settings.php"); // directs user to home page
                             exit;
                         } else {
