cf2a401de5e6d56ae1cc31fb4c57d0d428e08bb1
laurenhquan <laurenhquan@gmail.com>
2025-04-13 00:54:24 -0700

    started working on changing username, need to fix some errors with posting new_username

--- back-end/change-profile.php
+++ back-end/change-profile.php
@@ -1,17 +1,49 @@
 <?php
-    session_start();
-
-    $mysqli = require __DIR__ . "/database.php";
-
     function changeUser() {
-        $is_invalid = TRUE;
-        $is_taken = TRUE;
+        session_start();
 
-        if ($is_invalid) { echo "<em style='color: #c1121f;'>Incorrect password</em><br>"; }
-        if ($is_taken) { echo "<em style='color: #c1121f;'>User is already taken</em><br>"; }
+        $mysqli = require __DIR__ . "/database.php";
+
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+            if(!password_verify($_POST["pass"], $_SESSION["pass_hash"])) { // if password does not match the hash ver of it
+                echo "<em style='color: #c1121f;'>Incorrect password</em><br>";
+            }
+            else { // if password is correct
+                if($_POST["new_username"] == $_SESSION["username"]) { // if user puts in the same username already using
+                    echo "<em style='color: #c1121f;'>You are already using this username</em><br>";
+                }
+                else {
+                    $check_user = sprintf("SELECT * FROM users WHERE username = '%s'", $mysqli->real_escape_string($_POST["new_username"])); // query to check if username exists in db
+                    $result = $mysqli->query($check_user); // run query
+                    $existing_user = $result->fetch_assoc(); // variable to hold data if user exists
+
+                    if ($existing_user) { // if username already exists
+                        echo "<em style='color: #c1121f;'>Username is already taken</em><br>";
+                    }
+                    else { // if username is free
+                        $update_user = "UPDATE users SET username = ? WHERE id = ?"; // query to update username in db
+                        $stmt = $mysqli->stmt_init(); // create statement
+                        if(!$stmt->prepare($update_user)) { die("MySQL error: " . $mysqli->error); } // kill if error with preparing query
+                        $stmt->bind_param("si", $_POST["new_username"], $_SESSION['user_id']); // binds parameters
+
+                        if($stmt->execute()) { // execute statement
+                            $_SESSION["username"] = $_POST["new_username"]; // update session username
+                            header("Location: settings.php"); // directs user to home page
+                            exit;
+                        } else {
+                            die($mysqli->error . " " . $mysqli->errno); // kill if execute gives error
+                        }
+                    }
+                }              
+            }
+        }
     }
 
     function changePass() {
+        session_start();
+
+        $mysqli = require __DIR__ . "/database.php";
+
         $is_invalid = TRUE;
 
         if ($is_invalid) { echo "<em style='color: #c1121f;'>Incorrect password</em><br>"; }
